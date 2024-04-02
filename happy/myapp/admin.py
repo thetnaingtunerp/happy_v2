af7a967a216ae9f72197222f6af130cab28b92dc
thetnaing <kothetnaingtun.ict@gmaiil.com>
2024-04-02 13:47:31 +0630

    chanage adminpy

--- happy/myapp/admin.py
+++ happy/myapp/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(CartProduct)
+
+
+admin.site.register(pOrder)
+admin.site.register(pCartProduct)
